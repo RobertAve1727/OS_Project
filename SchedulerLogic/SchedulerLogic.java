55878e6ed9f673a655d5d540246fd88e25ad20db
Avelino <robertave1727@gmail.com>
2025-07-15 19:54:33 +0800

    added class Process that accepts the required inputs

---
+++ SchedulerLogic/SchedulerLogic.java
@@ -0,0 +1,18 @@
+//import java.util.*;
+
+class Process {
+    String pid;
+    int arrivalTime, burstTime, remainingTime;
+    int completionTime, turnaroundTime, waitingTime, responseTime;
+    boolean started = false;
+
+    public Process(String pid, int arrivalTime, int burstTime) {
+        this.pid = pid;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.remainingTime = burstTime;
+    }
+}
+public class SchedulerLogic {
+
+}
